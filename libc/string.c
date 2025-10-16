044679355c0927217913220757673e80a6c20ae7
Natneam M <se.natneam.mesele@gmail.com>
2025-10-16 22:45:26 +0300

    Add atoi function implementation to string library

--- libc/string.c
+++ libc/string.c
@@ -7,3 +7,12 @@ int strcmp(const char* s1, const char* s2) {
     }
     return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
+
+int atoi(char *p) {
+    int k = 0;
+    while (*p) {
+        k = (k << 3) + (k << 1) + (*p) - '0';
+        p++;
+     }
+     return k;
+}
