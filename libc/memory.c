733d7746ca74a6178e40ea9e9aa41ed068fb720e
Natneam M <se.natneam.mesele@gmail.com>
2025-10-24 23:05:45 +0300

    Implement memset function for memory initialization

--- libc/memory.c
+++ libc/memory.c
@@ -5,3 +5,11 @@ void memory_copy(char* source, char* dest, int no_bytes)
         *(dest + i) = *(source + i);
     }
 }
+
+void* memset(void* buf, int c, unsigned int len) {
+    unsigned char* p = buf;
+    while (len-- > 0) {
+        *p++ = (unsigned char)c;
+    }
+    return buf;
+}
--- libc/memory.h
+++ libc/memory.h
@@ -2,5 +2,6 @@
 #define MEMORY_H
 
 void memory_copy(char* source, char* dest, int no_bytes);
+void* memset(void* buf, int c, unsigned int len);
 
 #endif
